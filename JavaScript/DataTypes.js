95edd7962dcc7cb19dc705e00924b988c71c2e7f
Ashvani Sharma <ashvani.sharma_cs22@gla.ac.in>
2024-09-04 23:06:48 +0530

    complete

---
+++ JavaScript/DataTypes.js
@@ -0,0 +1,11 @@
+console.log("Data Type");
+var myFavNum=98;
+console.log(myFavNum);
+var str="Ashvani Sharma";
+console.log(str)
+var isRain=true;
+console.log(isRain)
+var badMem=null;
+console.log(badMem)
+const bigNum=1234567898483181174949n
+console.log(bigNum)
---
+++ JavaScript/Variable.js
@@ -0,0 +1,3 @@
+console.log("welcome to india~")
+var myAge=30
+console.log(myAge)
